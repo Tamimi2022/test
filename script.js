8b4c63cad4098174be20ef91ec35b5dced294dfa
Farid Attamimi <tamimi2021farid@gmail.com>
2022-04-03 11:31:29 +0200

    First using Github

---
+++ script.js
@@ -0,0 +1,32 @@
+// Part 1
+const age = 20;
+if (age >= 18 && age <= 25) {
+    console.log("You get 50% off!");
+}
+else {
+    console.log("You Not allowed!!!");
+}
+
+// Part 2
+const firstName = "sarah";
+if (firstName === "Bram" || firstName ==="sarah") {
+    console.log("You Got a Free Drink");
+}
+else {
+    console.log("You have to Pay!!");
+}
+
+// Part 3
+const totalAmount = 55;
+if (totalAmount > 100) {
+    console.log("you get a free bottle");
+}
+else if (totalAmount > 50) {
+    console.log("You get a free portion of nachos");
+}
+else if (totalAmount > 25) {
+    console.log("You get free (veggie) bitterballen");
+}
+else {
+    console.log("Good Bye!!")
+}
